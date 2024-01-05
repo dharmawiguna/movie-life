11cfd00a030fe0f205b0193bf97fc31f930144be
dharmawiguna <dharmawiguna982@gmail.com>
2024-01-05 10:26:55 +0800

    login integration

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ use Inertia\Inertia;
 //     ]);
 // });
 
-Route::redirect('/', '/prototype/login');
+Route::redirect('/', '/login');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -53,10 +53,10 @@ Route::prefix('prototype')->name('prototype.')->group( function () {
     })->name('movie.detail');
 });
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
